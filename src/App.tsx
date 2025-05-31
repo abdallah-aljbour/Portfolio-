19da20f2e551ad3d7dcf3c92ac0a291c5938f0a8
unknown <abdallaaljbour03@gmail.com>
2025-05-31 14:20:16 +0300

    add OG

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import HeroSection from "./components/sections/Hero";
+import ComingSoon from "./components/sections/ComingSoon";
 function App() {
   return (
     <>
       <HeroSection />
+      <ComingSoon />
     </>
   );
 }
