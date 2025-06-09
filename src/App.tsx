fb2cc292a7d386b8f42e63978f1eb801f880b03c
unknown <abdallaaljbour03@gmail.com>
2025-06-09 17:55:27 +0300

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+import Header from "./components/layout/Header";
 import HeroSection from "./components/sections/Hero";
 import AboutMe from "./components/sections/About";
 import Projects from "./components/sections/Projects";
 import Skills from "./components/sections/Skills";
 import Contact from "./components/sections/Contact";
+import "./styles/main.scss";
+
 function App() {
   return (
     <>
-      <HeroSection />
-      <AboutMe />
-      <Projects />
-      <Skills />
-      <Contact />
+      <Header />
+      <main>
+        <section id="home">
+          <HeroSection />
+        </section>
+        <section id="about">
+          <AboutMe />
+        </section>
+        <section id="services">
+          <Skills />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </main>
     </>
   );
 }
